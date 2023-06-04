054a3fc7cac6ecae4f46ee4d7bd4a2515d2cd45d
Kerem PEKARLI <kerempekarli@gmail.com>
2023-06-04 16:19:47 +0300

    stripe

--- v1/src/controllers/order.js
+++ v1/src/controllers/order.js
@@ -1,11 +1,36 @@
 const cart = require("../services/cart");
 const { orderCartİtems } = require("../services/order");
+require("dotenv").config();
+const stripe = require("stripe")(process.env.STRIPE_SECRET_TEST);
 
 const order = async (req, res) => {
   const cartId = await cart.getOrCreateCart(req.user.id);
   const cartItems = await cart.getCartItems(cartId);
   const result = await orderCartİtems(req.user.id, cartItems);
 
-  res.status(200).send({ result: result });
+  const { amount, id } = req.body;
+
+  try {
+    const payment = await stripe.paymentIntents.create({
+      amount,
+      currency: "USD",
+      description: "Spatula denem 1510",
+      payment_method: id,
+      confirm: true,
+    });
+
+    console.log(payment);
+    res.status(200).json({
+      message: "Satın alma işlemi başarılı",
+      success: true,
+    });
+  } catch (err) {
+    console.log("ERROR ", err);
+    res.json("Satın alma işlemi başarısız");
+  }
+
+  console.log("AMOUNT AND ID ", amount, " ", id);
+
+  // res.status(200).send({ success: true, message: "başarılı" });
 };
 module.exports = { order };
